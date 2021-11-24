94e4a6b3f54f0c2bf95424541a8b34e70df58270
DaanVersavel <92854110+DaanVersavel@users.noreply.github.com>
2021-11-24 19:50:52 +0100

    Database

--- routes/Contact.js
+++ routes/Contact.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+//Require our controllers.
+var Contact_controller = require('../controllers/ContactController');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('Contact', { title: 'Express' });
 });
+
+//post
+router.post('/', Contact_controller.vraag_create_post );
+
+
 
 module.exports = router;
