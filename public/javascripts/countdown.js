f37672905e520150671a918e8ff4c235157fd522
Siemen <siemen.vanderhoeven16@gmail.com>
2021-11-30 11:23:11 +0100

    update fotonamen en countdown

--- public/javascripts/countdown.js
+++ public/javascripts/countdown.js
@@ -1,20 +1,26 @@
 var nvdjTijd = new Date('21 oktober 2022 20:00:00').getTime();
 
 //update countdowntijd
-function updateCountdown(){
+var update = setInterval(function (){
     var nu = new Date().getTime();
     var verschil = nvdjTijd - nu;
-
-    var d = Math.floor(verschil/1000/60/60/24);
-    var h = Math.floor(verschil/1000/60/60)%24;
-    var m = Math.floor(verschil/1000/60)%60;
-    var s = Math.floor(verschil/1000)%60;
-
-    document.getElementById("days" ).innerHTML =d;
-    document.getElementById("hours" ).innerHTML = h<10?'0'+h:h;     //als h<10 zetteen we 0h, anders gewoon h
-    document.getElementById("minutes" ).innerHTML = m<10?'0'+m:m;   //als m<10 zetteen we 0m, anders gewoon m
-    document.getElementById("seconds" ).innerHTML = s<10?'0'+s:s;   //als s<10 zetteen we 0s, anders gewoon s
-
-
-}
-setInterval(updateCountdown,1000);
+    //omzetting naar dagen, uren, minuten en seconden
+    var d = Math.floor(verschil/(1000*60*60*24));
+    var h = Math.floor((verschil % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var m = Math.floor((verschil % (1000 * 60 * 60)) / (1000 * 60));
+    var s = Math.floor((verschil % (1000 * 60)) / 1000);
+    //plaats nummers in countdown
+    document.getElementById("days").innerHTML = d;
+    document.getElementById("hours").innerHTML = h//<10?'0'+h:h;     //als h<10 zetteen we 0h, anders gewoon h
+    document.getElementById("minutes").innerHTML = m//<10?'0'+m:m;   //als m<10 zetteen we 0m, anders gewoon m
+    document.getElementById("seconds").innerHTML = s//<10?'0'+s:s;   //als s<10 zetteen we 0s, anders gewoon s
+    //countdown finished
+    if(veschil < 0){
+        clearInterval(update);
+        document.getElementById("days").innerHTML = "00 ";
+        document.getElementById("hours").innerHTML = "00 ";
+        document.getElementById("minutes").innerHTML = "00 ";
+        document.getElementById("seconds").innerHTML = "00 ";
+    }
+    console.log(m)
+},1000);
